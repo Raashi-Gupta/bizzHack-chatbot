8dee062f442227d77cc99979bfe1a81143087e71
Raashi-Gupta <raashig2010@gmail.com>
2025-07-28 17:23:06 +0530

    code updated

--- src/app/most-searched/most-searched.component.ts
+++ src/app/most-searched/most-searched.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { operationList } from '../shared/operationList';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
@@ -10,47 +10,42 @@ import { Router } from '@angular/router';
   styleUrl: './most-searched.component.scss'
 })
 
-export class MostSearchedComponent {
+export class MostSearchedComponent implements OnInit{
   operations = operationList;
-  selectedTab = '';
+  selectedBusiness : string = '';
   questionList: any;
   constructor(private router: Router) { }
+  ngOnInit(): void {
+    const sb = localStorage.getItem('selectedBusiness')
+    if(sb) this.selectedBusiness = sb;
+  }
 
   navigateTo(path: string) {
     if (path) {
       this.router.navigate([path]);
     }
   }
-  selectTab(tab: string) {
-    this.selectedTab = tab
-    this.questionList = this.questionsMap[this.selectedTab] || [];
-  }
+  // selectTab(tab: string) {
+  //   this.selectedTab = tab
+  //   this.questionList = this.questionsMap[this.selectedTab] || [];
+  // }
 
   //hardcodeQuestions
   questionsMap: Record<string, string[]> = {
-    "Production": [
-      'What is the Data integration and processing layers of TrendyThreads',
-      'What are data sources of TrendyThreads'
-      
-    ],
-    "Maintenance": [
-      'What is the Data integration and processing layers of TrendyThreads',
-      'What are BI Tools'
-  
-    ],
-    "Safety": [
+    "EHS Safety Regulations": [
+      'What to do incase of building fire?',
+      'What to do if you inhale chemical',
+      'What is Customer relationship management',
+      'What are Data warehouse'],
+      "Tata Motors": [
+        'What is the Data integration and processing layers of TrendyThreads',
+        'What are data sources of TrendyThreads',
+        'What is the Data integration and processing layers of TrendyThreads',
+        'What are BI Tools',
         'What is Evacuation Plan',
         'What are Emergency Numbers',
-        'What to do incase of building fire?',
-        'What to do if you inhale chemical'
-  
-    ],
-    "Procurement": [
-       'What is Customer relationship management',
-       'What are Data warehouse'
-    ],
-  };
-
+      ]
+    };
   redirectToChat(question: string) {
     this.router.navigate(['/chat'], {
       queryParams: { question: question }
